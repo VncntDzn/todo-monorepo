91628ba072941d3e9ecaaf79851587652d7e43aa
Vincent Dizon <workvpdizon@gmail.com>
2025-02-06 00:21:11 +0800

    setup web project

---
+++ apps/web/src/app/providers/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router";
+import { router } from "../routes";
+
+const queryClient = new QueryClient();
+
+export const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
