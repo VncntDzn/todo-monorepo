0cdefd4539ccf63cbf87a808795747b0aa38d465
Vincent Dizon <workvpdizon@gmail.com>
2025-02-19 21:54:18 +0800

    added routes

--- apps/web/src/app/routes/index.tsx
+++ apps/web/src/app/routes/index.tsx
@@ -1,9 +1,21 @@
-import { createBrowserRouter } from "react-router";
 import { Home } from "@/pages/home";
+import { MyDay } from "@/pages/home/ui/my_day-tasks.page";
+import { PlannedTasks } from "@/pages/home/ui/planned-tasks.page";
+import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+    children: [
+      {
+        path: "/my-day",
+        Component: MyDay,
+      },
+      {
+        path: "/planned-tasks",
+        Component: PlannedTasks,
+      },
+    ],
   },
 ]);
---
+++ apps/web/src/pages/home/ui/my_day-tasks.page.tsx
@@ -0,0 +1,3 @@
+export const MyDay = () => {
+  return <div>MyDay</div>;
+};
